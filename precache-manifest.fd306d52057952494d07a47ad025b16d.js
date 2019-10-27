53acfbb978fa59978219efb05608d9358fe2afdf
ruvalter <ruvalter@gmail.com>
2019-10-27 15:13:20 -0400

    Updates

---
+++ precache-manifest.fd306d52057952494d07a47ad025b16d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211ec578c96b1d3bd14052fb7a75760a",
+    "url": "/burguer-builder/index.html"
+  },
+  {
+    "revision": "7945fb4df34f612cddc2",
+    "url": "/burguer-builder/static/css/main.afd754fc.chunk.css"
+  },
+  {
+    "revision": "c8e8dff0f971e50191c0",
+    "url": "/burguer-builder/static/js/2.317d62e3.chunk.js"
+  },
+  {
+    "revision": "7945fb4df34f612cddc2",
+    "url": "/burguer-builder/static/js/main.8d96ed76.chunk.js"
+  },
+  {
+    "revision": "fe7debbed4bf5a31d525",
+    "url": "/burguer-builder/static/js/runtime-main.9e8983c6.js"
+  }
+]);
